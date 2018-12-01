82caeabefb8c2fc881f786ce7d6e6cc0c2ea8a49
zolitisslagon <45512483+zolitisslagon@users.noreply.github.com>
2018-12-01 13:36:02 +0100

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+      <script>
+       const mySwiper = new Swiper('.swiper-container', {
+           speed: 400,
+           pagination: {
+               el: '.swiper-pagination',
+               type: 'bullets',
+               clickable: true
+           }
+       });
+   </script>
+   <script src="./js/swiper.min.js">
+   </script>
---
+++ teachers.md
@@ -0,0 +1,4 @@
+# Semaine 5 - Javascript
+
+- Montrer du Javascript pour voir la différence avec l'HTML et le CSS
+- Pour le dernier exercice, faire du pas à pas pour faire comprendre la logique algorithmique de ce code
